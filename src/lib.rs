67fbe3b3bebe2b17fd52aa7fbfa477ea1504d012
xd009642 <danielmckenna93@gmail.com>
2018-10-08 22:23:26 +0100

    Started things going

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+extern crate nix;
+extern crate mach;
+
+pub fn try_it_out() {
+    println!("Trying out an experiment");
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+extern crate apple_ptrace_test;
+
+use apple_ptrace_test::*;
+
+fn main() {
+    try_it_out();
+}
