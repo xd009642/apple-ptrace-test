dcfae950d5e0cff0d455a58c67f82cef9f1b60c8
xd009642 <danielmckenna93@gmail.com>
2018-10-08 22:44:14 +0100

    Added some mach hacks

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 extern crate nix;
 extern crate mach;
 
+use std::mem;
+
 use nix::sys::ptrace;
 use nix::sys::signal::{raise, Signal};
 use nix::sys::wait::{waitpid, WaitStatus};
 use nix::unistd::fork;
 use nix::unistd::ForkResult::*;
+
+use mach::traps::{mach_task_self, task_for_pid};
+use mach::mach_port::*;
+use mach::port::*;
 
 pub fn try_it_out() {
     println!("Trying out an experiment");
@@ -17,11 +23,17 @@ pub fn try_it_out() {
             }
         },
         Parent{child} => {
+            unsafe {
+            let mut port: mach_port_name_t =  mem::uninitialized() ;
+            task_for_pid(mach_task_self(), child.into(), port as *mut mach_port_name_t);
             assert_eq!(waitpid(child, None), Ok(WaitStatus::Stopped(child, Signal::SIGTRAP)));
             ptrace::cont(child, None).unwrap();
             assert_eq!(waitpid(child, None), Ok(WaitStatus::Stopped(child, Signal::SIGTRAP)));
             ptrace::cont(child, Signal::SIGKILL).unwrap();
             // child dead
+            //
+            mach_port_deallocate(mach_task_self(), port);
+            }
         }
     }
     println!("If you read this I've worked");
